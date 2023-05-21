4d6d36544d9775332b628dd5f38385f8d90d8393
nobleosinachi <NOBLEOSINACHI98@GMAIL.COM>
2023-05-21 17:05:51 +0100

    Added Read and Write functions

---
+++ Marketplace.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Marketplace {
+    string internal product = "Burger";
+
+    function writeProduct(string memory _product) public {
+        product = _product;
+    }
+
+    function readProduct() public view returns (string memory) {
+        return product;
+    }
+}
